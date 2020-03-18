d6bd41a323e366561f9fbe8ad404f3c2edddd2da
Tanner Williams <tannerwill756@gmail.com>
2020-03-18 18:02:58 -0400

    nasa image and info is displaying properly

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,13 +1,15 @@
 import React from "react";
+import NasaList from "./NasaList";
 
 const NasaCard = props => {
-
+    console.log(props)
     return (
         <div className = "nasaCard">
-            <h1>Title</h1>
-            <p>Date</p>
-            <img src="" alt="">Image HERE</img>
-            <p>explanation</p>
+            <h1>{props.title}</h1>
+            <p>{props.date}</p>
+            <img src={props.img} alt="" />
+            <p>{props.explan}</p>
+            <p>Copyright {props.copyright}</p>
         </div>
     )
 }
